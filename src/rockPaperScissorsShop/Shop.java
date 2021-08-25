b4d7bc0ea933a91b8392cdb6b99532fc472bcca9
bticamp <bticamp@DESKTOP-H0IJBRQ>
2021-08-25 18:32:55 +0900

    210825

--- src/rockPaperScissorsShop/Shop.java
+++ src/rockPaperScissorsShop/Shop.java
@@ -1,97 +1,109 @@
 package rockPaperScissorsShop;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 
-class ShopMenuSwitch {
-	
-	static Scanner scan = null;
-	
+public class Shop {
+	// 스캐너 sc로 통일했음
 	static String bookName = "책이름";
 	static int bookCoin = 0;
+	static Scanner sc = new Scanner(System.in); // 전역으로 선언하면 다 쓸수 있다
+	static int result; // 전역으로 선언
+	
+	// 메인 메소드
+	public static void main(String[] args) {
+		
+		selectMenu();
+	}
+
+	public static void selectMenu() { // 메소드로 묶어주었다 (호출하면 반복하게)
+		// 1. 아이템 선택 메뉴 전시
+		while(true) {
+			try {
+				shopMain();
+				result = Integer.parseInt(sc.nextLine());
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println(" > 에러 : 입력 에러. 숫자를 입력하세요. \n\n\n\n");
+				continue;
+			} catch (InputMismatchException e) {
+				System.out.println(" > 에러 : asdfasdf입력 에러. 숫자를 입력하세요. \n\n\n\n");
+				continue;
+			}
+		}
+		
+		switch (result) {
+		case 1 : menuSwitch("혼자 공부하는 자바", 10); break;
+		case 2 : menuSwitch("자바스크립트 + 제이쿼리 입문", 10); break;
+		case 3 : menuSwitch("점프 투 파이썬", 20); break;		
+		case 4 : menuSwitch("스프링 퀵 스타트", 20); break;
+		case 5 : menuSwitch("개발자", 99); break;
+		case 0 :
+			System.out.println(" > 메인 화면으로 돌아갑니다.");
+			selectMenu();
+			break;	
+		}
+	}
+	    
+
+	public static void shopMain() {
+		
+		// JAVA SHOP으로 들어가면 나오는 메뉴
+		System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦ ");
+		System.out.println();
+		System.out.println(" 	[ 가위바위보 코인을 모아 웹개발 책 아이템을 구입해 보세요~ ] ");
+		System.out.println();
+		System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦");
+		System.out.println();
+		System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦ ");
+		System.out.println();
+		System.out.println(" 1. 혼자 공부하는 자바 (10코인)	2. 자바스크립트 + 제이쿼리 입문 (10코인) ");
+		System.out.println(" 3. 점프 투 파이썬 (20코인)		4. 스프링 퀵 스타트 (20코인) ");
+		System.out.println(" 5. 개발자 (99코인) ");
+		System.out.println();
+		System.out.println(" 			0. 메인화면으로		 ");
+		System.out.println();
+		System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦");
+		System.out.print(" > ");
+	}
+	
+	
+	
 	
 	
 	static void menuSwitch(String bookName, int bookCoin) {
 		
-	System.out.println(" > [" + bookName + "]은(는) [" + bookCoin + "코인]입니다. 구입할까요? (현재 잔여 코인 : " + ")");
+	System.out.println(" > [" + bookName + "]은(는) [" + bookCoin + "코인]입니다. 구입할까요? (현재 잔여 코인 : " +  ")"); // coin 연결하기
 	System.out.println("		☞ 1. YES		☞ 2. NO		");
-	String yesOrNoSc = scan.nextLine(); // yesOrNo 주석처리했는데 왜돌아가냐고 이거
-		if (Integer.parseInt(yesOrNoSc) == 1) { 
-			System.out.println("-------------------------------------------------------------");
-			System.out.println();
-			System.out.println("> [" + bookName + "]를 구입했습니다. (현재 잔여 코인 : " + ")");
+	System.out.print(" > ");
+	
+		while (true) {
 			try {
-				System.out.println();
-				System.out.println(" > 3초 후 상점 화면으로 돌아갑니다.\n\n\n\n\n\n");
-				Thread.sleep(3000); // 3초 지연 해부리기
-				ShopMain.shopMain();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			
-		} else {
-			System.out.println(" > 상점 화면으로 돌아갑니다.\n\n\n\n\n\n");
-			ShopMain.shopMain();
-//			break;
-		}
-		
-//		scan.close(); // 근데 이거 안닫아도 되나? 닫으면 스캐너 꺼져서 에러남
-		
-	}
-}
-
-
-
-public class Shop {
-	public static void main(String[] args) {
-		
-		ShopMenuSwitch swh = new ShopMenuSwitch();
-		swh.scan = new Scanner(System.in);
-				
-		
-		ShopMain.shopMain();
-		
-		
-		while(true) {
-			
-			String sNum = swh.scan.nextLine();
-
-
-			switch(sNum) {
-			case "1" : ShopMenuSwitch.menuSwitch("혼자 공부하는 자바", 10); break;
-			
-			case "2" : ShopMenuSwitch.menuSwitch("자바스크립트 + 제이쿼리 입문", 10); break;
-				
-				
-			case "3" : ShopMenuSwitch.menuSwitch("점프 투 파이썬", 20); break;
-				
-				
-			case "4" : ShopMenuSwitch.menuSwitch("스프링 퀵 스타트", 20); break;
-				
-				
-			case "5" : ShopMenuSwitch.menuSwitch("개발자", 99); break;
-				
-				
-			case "0" :
-				System.out.println(" > 메인 화면으로 돌아갑니다.");
-				try {
+				if (Integer.parseInt(sc.nextLine()) == 1) { 
+					// 여기서 코인 차감하는 코드넣기 (어쩌구 = 현재코인 - 코인값)
+					System.out.println("-------------------------------------------------------------");
 					System.out.println();
-					Thread.sleep(3000); // 3초 지연
-					ShopMain.shopMain(); // 여기다 메인화면으로 들어가는 뭐시기 호출해서 넣기
-				} catch (InterruptedException e) {
-					e.printStackTrace();
+					System.out.println("> [" + bookName + "]를 구입했습니다. (현재 잔여 코인 : " + ")");
+					try {
+						System.out.println();
+						System.out.println(" > 3초 후 상점 화면으로 돌아갑니다.\n\n\n\n\n\n");
+						Thread.sleep(3000); // 3초 지연 해부리기
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
 				}
-			
-				
-				break;
 				
 				
-			
+				selectMenu();
+				
+				
+	//			break;
+				
+			} catch (NumberFormatException e) {
+				System.out.println(" > 에러 : 문자열은 입력될 수 없습니다. 숫자를 입력하세요. \n\n\n\n");
+				continue;
 			}
 		}
-		
-		
 	}
-	    
-
 }
--- src/rockPaperScissorsShop/ShopMain.java
+++ src/rockPaperScissorsShop/ShopMain.java
@@ -1,45 +1,53 @@
 package rockPaperScissorsShop;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class ShopMain {
+	Scanner scan = new Scanner(System.in);
 		
-		public static void shopSc() {
+//		public static void shopSc() {
 			
-			Scanner scan = new Scanner(System.in);
+			
+			
+//			while (!scan.hasNextInt()) { // 값이 숫자인지 판별하기
+//				scan.next(); // 값이 숫자가 아니면 버림
+//				System.err.print(" > 에러 : ff문자열은 입력될 수 없습니다. 숫자를 입력하세요. \n\n\n\n");
+//				scan.nextInt(); // 위에서 값이 숫자일경우 이쪽으로 값을 넣는다
+//			}
 			
 			
 			// 그뭐냐...필드인가 생성잔가 자주쓰는거 넣어보자
 //			String yesOrNo = scan.nextLine(); // 예쓰 노를 선택하는건데 중복되는것같으니 나중에 메소드인가 클래스로 빼기
 			// 뭐야 이거 주석처리했는데 왜돌아가는거임?????왜????
 			
-		}
+//		}
 		
 		
 		
 		
 		
 		// 여기다가 상점 메인 메시지 넣기 (뭐시기를 구입해보세요~~ 같은거)
-		public static void shopMain() {
-			
-			// JAVA SHOP으로 들어가면 나오는 메뉴
-			System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦ ");
-			System.out.println();
-			System.out.println(" 	[ 가위바위보 코인을 모아 웹개발 책 아이템을 구입해 보세요~ ] ");
-			System.out.println();
-			System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦");
-			System.out.println();
-			System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦ ");
-			System.out.println();
-			System.out.println(" 1. 혼자 공부하는 자바 (10코인)	2. 자바스크립트 + 제이쿼리 입문 (10코인) ");
-			System.out.println(" 3. 점프 투 파이썬 (20코인)		4. 스프링 퀵 스타트 (20코인) ");
-			System.out.println(" 5. 개발자 (99코인) ");
-			System.out.println();
-			System.out.println(" 			0. 메인화면으로		 ");
-			System.out.println();
-			System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦");
-			
-		}
-		
+		/*
+		 * public static void shopMain() {
+		 * 
+		 * // JAVA SHOP으로 들어가면 나오는 메뉴
+		 * System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦ ");
+		 * System.out.println();
+		 * System.out.println(" 	[ 가위바위보 코인을 모아 웹개발 책 아이템을 구입해 보세요~ ] ");
+		 * System.out.println();
+		 * System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦");
+		 * System.out.println();
+		 * System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦ ");
+		 * System.out.println();
+		 * System.out.println(" 1. 혼자 공부하는 자바 (10코인)	2. 자바스크립트 + 제이쿼리 입문 (10코인) ");
+		 * System.out.println(" 3. 점프 투 파이썬 (20코인)		4. 스프링 퀵 스타트 (20코인) ");
+		 * System.out.println(" 5. 개발자 (99코인) "); System.out.println();
+		 * System.out.println(" 			0. 메인화면으로		 "); System.out.println();
+		 * System.out.println(" ▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦▦");
+		 * System.out.print(" > ");
+		 * 
+		 * }
+		 */
 
 }
